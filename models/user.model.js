2ec51bdac754e08e26449efd4147231055090174
DeKingoye <yannmouandza3@gmail.com>
2024-06-12 10:28:55 +0200

    authentication with jwt

--- models/user.model.js
+++ models/user.model.js
@@ -61,6 +61,18 @@ userSchema.pre("save", async function(next) {
     next();
 } )
 
+userSchema.statics.login = async function(email, password){
+    const user = await this.findOne({email});
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if(auth){
+            return user; 
+        }
+        throw Error('incorrect password')
+    } 
+    throw Error('incorrecr email')
+}
+
 const UserModel = mongoose.model('user', userSchema); 
 
 module.exports = UserModel;
